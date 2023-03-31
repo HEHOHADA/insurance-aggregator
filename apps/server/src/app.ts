3278e51730a9dcfc6f21fc604a6d39d8819852f3
HEHOHADA <bulat5512@yandex.ru>
2023-03-31 12:56:26 +0300

    feat: added prisma ORM

---
+++ apps/server/src/app.ts
@@ -0,0 +1,34 @@
+import type { FastifyServerOptions } from "fastify";
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import cookie from "@fastify/cookie";
+
+import { prismaPlugin } from "./db";
+
+export type AppOptions = Partial<FastifyServerOptions>;
+
+async function buildApp(_options: AppOptions = {}) {
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  fastify.register(prismaPlugin);
+  //#region fastify plugins
+
+  fastify.register(cors, {
+    origin: ["http://localhost:3000", "http://localhost:4000"],
+    credentials: true,
+  });
+
+  fastify.register(cookie, {
+    secret: "my-secret",
+  });
+
+  fastify.get("/status", async (req, res) => {
+    res.code(200).send({ message: "Ok" });
+  });
+
+  return fastify;
+}
+
+export { buildApp };
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,46 +1,21 @@
-import cookie from "@fastify/cookie";
-import cors from "@fastify/cors";
-import Fastify from "fastify";
 import * as dotenv from "dotenv";
+import { buildApp } from "./app";
 
 dotenv.config();
 
 const port = (process.env.SERVER_PORT as unknown as number) || 3009;
 const host = process.env.SERVER_HOST || "localhost";
 
-const fastify = Fastify({
-  logger: true,
-});
-
-//#region fastify plugins
-
-fastify.register(cors, {
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:4000",
-    "https://local.dev.acquirecloud.io:3000",
-    /\.acquirecloud\.io$/,
-  ],
-  credentials: true,
-});
-
-fastify.register(cookie, {
-  secret: "my-secret",
-  parseOptions: {},
-});
-
-fastify.get("/status", async (req, res) => {
-  res.code(200).send({ message: "Ok" });
-});
-
 const start = async () => {
+  const app = await buildApp();
+
   try {
-    await fastify.listen({
+    await app.listen({
       port,
       host,
     });
   } catch (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
 };
