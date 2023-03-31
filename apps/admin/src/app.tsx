f88399d086c6c26d2250035ec272c1710c2b908d
HEHOHADA <bulat5512@yandex.ru>
2023-03-31 23:49:40 +0300

    feat(admin): add admin app
    Add admin app with basic configuration and setup. The app uses SolidJS
    and TailwindCSS. It includes a README file with instructions on how to
    install and run the app. The app has a basic header with a link to the
    SolidJS GitHub page. The app is built using Vite and can be deployed to
    any static host provider. The app has a .gitignore file that ignores
    the dist folder.

---
+++ apps/admin/src/app.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div>
+      <header>
+        <a
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
