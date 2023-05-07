3f8b034266fa19af01627a8ff61a64e85261d628
HEHOHADA <bulat5512@yandex.ru>
2023-05-08 02:40:42 +0300

    feat: serverless deploy

--- apps/admin/src/pages/home/index.ts
+++ apps/admin/src/pages/home/index.ts
@@ -4,6 +4,7 @@ import { Loader } from "../../shared/ui";
 import { currentRoute } from "./model";
 import { HomePage } from "./page";
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
 export const HomeRoute = {
   view: createRouteView({ route: currentRoute, view: HomePage, otherwise: Loader }),
   route: currentRoute,
